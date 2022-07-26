0db3d0270ddf0a1552900ebb5552634e13a10e98
8acebuzzy <108508774+8acebuzzy@users.noreply.github.com>
2022-07-26 10:40:59 +0000

    rustlings

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -6,27 +6,40 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
-
 use std::sync::Arc;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+
+use std::sync::atomic::{AtomicU32, Ordering};
 
 struct JobStatus {
-    jobs_completed: u32,
+    jobs_completed: AtomicU32,
 }
-
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(JobStatus { jobs_completed: AtomicU32::new(0), });
     let status_shared = status.clone();
+    let now = Instant::now();
+
+    macro_rules! dbg_time {
+        ($i:ident) => {{
+            let $i = now.elapsed().as_millis();
+            dbg!($i);
+        }};
+    }
+
     thread::spawn(move || {
+        dbg_time!(before_for);
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
-            status_shared.jobs_completed += 1;
+            status_shared.jobs_completed.fetch_add(1, Ordering::SeqCst);
+            dbg_time!(in_for);
         }
+        dbg_time!(after_for);
     });
-    while status.jobs_completed < 10 {
+    while status.jobs_completed.load(Ordering::SeqCst) < 10 {
         println!("waiting... ");
+        dbg_time!(load); 
         thread::sleep(Duration::from_millis(500));
+        dbg_time!(sleep);
     }
 }
