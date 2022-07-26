8c9cb6b27e5a54836b71dcb12c25d0944db954ad
8acebuzzy <108508774+8acebuzzy@users.noreply.github.com>
2022-07-26 10:16:02 +0000

    rustlings

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,7 +2,7 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-/#[cfg(test)]
+#[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
